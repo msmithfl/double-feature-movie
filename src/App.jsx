9a64e1c7472a012bfec2b1adb45617641dd2b861
Matthew Smith <msmithfl@icloud.com>
2023-07-30 10:31:07 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center mt-5">
+        Tonight's Double Feature
+      </h1>
+    </>
+  );
+}
+
+export default App;
